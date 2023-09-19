47323281d062e310b089f021bc8dce10db19248e
datastohne <stijn.uijen@databricks.com>
2023-09-19 14:30:02 +0200

    test

--- retrieve-baby-names.py
+++ retrieve-baby-names.py
@@ -4,3 +4,5 @@ import requests
 response = requests.get('http://health.data.ny.gov/api/views/myeu-hzra/rows.csv')
 csvfile = response.content.decode('utf-8')
 dbutils.fs.put("dbfs:/FileStore/babynames.csv", csvfile, True)
+
+
